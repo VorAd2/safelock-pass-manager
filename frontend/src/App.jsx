bb574de07f90bdb9c0c32a86f39ced51bf9af7ab
Vitor <adrianovor@gmail.com>
2025-09-22 10:34:31 -0300

    Rota para paginas não encontradas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 //f5 esta reconstruindo a arvore de componentes inteira: context é volatil
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HOME_ROUTE, SIGNIN_ROUTE, SIGNUP_ROUTE, DASHBOARD_ROUTE } from './routes';
-import {HomePage, DashboardWrapper, SigninPage, SignupPage} from './pages';
+import {HomePage, DashboardWrapper, SigninPage, SignupPage, NotFoundPage} from './pages';
 import {
   VaultsContent, SendContent, CardsContent, 
   ReceiptsContent, SettingsContent, ContactUsContent,
@@ -13,21 +13,22 @@ function App() {
   return (
     <Router>
        <Routes>
-          <Route path={HOME_ROUTE} element={<HomePage/>}/>
-          <Route path={SIGNIN_ROUTE} element={<SigninPage/>}/>
-          <Route path={SIGNUP_ROUTE} element={<SignupPage/>}/>
+          <Route path={HOME_ROUTE} element={<HomePage/>} />
+          <Route path={SIGNIN_ROUTE} element={<SigninPage/>} />
+          <Route path={SIGNUP_ROUTE} element={<SignupPage/>} />
 
           <Route path={DASHBOARD_ROUTE} element={<DashboardWrapper/>}>
-            <Route index element={<IndexContent />} /> 
-            <Route path="vaults" element={<VaultsContent />} />
-            <Route path="send" element={<SendContent />} />
-            <Route path="cards" element={<CardsContent />} />
-            <Route path="receipts" element={<ReceiptsContent />} />
-            <Route path="settings" element={<SettingsContent />} />
-            <Route path="contactus" element={<ContactUsContent />} />
+            <Route index element={<IndexContent/>} /> 
+            <Route path="vaults" element={<VaultsContent/>} />
+            <Route path="send" element={<SendContent/>} />
+            <Route path="cards" element={<CardsContent/>} />
+            <Route path="receipts" element={<ReceiptsContent/>} />
+            <Route path="settings" element={<SettingsContent/>} />
+            <Route path="contactus" element={<ContactUsContent/>} />
             <Route path="tools" element={<div>Tools Content here!</div>} /> 
           </Route>
 
+        <Route path='*' element={<NotFoundPage/>}/>
        </Routes>
     </Router>
   );
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+function NotFoundPage() {
+    return (
+        <div className="d-flex min-vh-100 justify-content-center align-items-center">
+            <div className="d-flex flex-column" style={{color:'whitesmoke'}}>
+                <h1>404 - Page Not Found</h1>
+                <p>The page you are looking for does not exist. Please, try again.</p>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundPage
